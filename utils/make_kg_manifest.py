4f9d4565980d1116712a6dc5c0a621fdcb94bcad
caufieldjh <j.harry.caufield@gmail.com>
2022-01-24 14:59:47 -0500

    Get list of all bucket object keys

--- utils/make_kg_manifest.py
+++ utils/make_kg_manifest.py
@@ -9,6 +9,8 @@ schema as defined here:
 https://github.com/linkml/linkml-model/blob/main/linkml_model/model/schema/datasets.yaml"""
 
 import boto3
+import botocore.exceptions
+import botocore.errorfactory
 import click
 
 @click.command()
@@ -16,8 +18,31 @@ import click
                required=True,
                nargs=1,
                help="""The name of an AWS S3 bucket to generate Manifest for.""")
-def run(bucket):
-    print(bucket)
+def run(bucket: str):
+    try:
+        keys = list_bucket_contents(bucket)
+    except botocore.exceptions.NoCredentialsError:
+        print("Can't find AWS credentials.")
+
+def list_bucket_contents(bucket: str):
+    """Lists all contents, keys only, of an AWS S3 bucket.
+    :param bucket: name of the bucket
+    :return: list of all keys, as strings, in the bucket"""
+    
+    all_object_keys = []
+
+    client = boto3.client('s3')
+
+    pager = client.get_paginator("list_objects_v2")
+
+    for page in pager.paginate(Bucket=bucket):
+        remote_contents = page['Contents']
+        for key in remote_contents:
+            all_object_keys.append(key['Key'])
+
+    print(f"Bucket {bucket} contains {len(all_object_keys)} objects.")
+
+    return all_object_keys
 
 if __name__ == '__main__':
   run()

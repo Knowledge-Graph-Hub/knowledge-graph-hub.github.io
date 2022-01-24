d0a4038940db4c63e3de62d5218f8eac8ece6218
caufieldjh <j.harry.caufield@gmail.com>
2022-01-24 13:55:07 -0500

    Init make_kg_manifest utility

---
+++ utils/make_kg_manifest.py
@@ -0,0 +1,23 @@
+# make_kg_manifest.py
+
+"""Utility for generating KG-Hub Manifest file,
+containing a list of all graphs and subgraphs
+(i.e., full KGs and graphs of transformed sources)
+for all KG-Hub projects.
+The Manifest file follows the LinkML Datasets
+schema as defined here:
+https://github.com/linkml/linkml-model/blob/main/linkml_model/model/schema/datasets.yaml"""
+
+import boto3
+import click
+
+@click.command()
+@click.option("--bucket",
+               required=True,
+               nargs=1,
+               help="""The name of an AWS S3 bucket to generate Manifest for.""")
+def run(bucket):
+    print(bucket)
+
+if __name__ == '__main__':
+  run()

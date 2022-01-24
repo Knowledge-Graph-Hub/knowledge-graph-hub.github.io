85cf012685d629d59b56b9d60a9ebf8d9dbd723a
caufieldjh <j.harry.caufield@gmail.com>
2022-01-24 15:25:21 -0500

    Get bucket keys resembling graphs

--- utils/make_kg_manifest.py
+++ utils/make_kg_manifest.py
@@ -21,6 +21,7 @@ import click
 def run(bucket: str):
     try:
         keys = list_bucket_contents(bucket)
+        graph_file_keys = get_graph_file_keys(keys)
     except botocore.exceptions.NoCredentialsError:
         print("Can't find AWS credentials.")
 
@@ -35,6 +36,7 @@ def list_bucket_contents(bucket: str):
 
     pager = client.get_paginator("list_objects_v2")
 
+    print(f"Searching {bucket}...")
     for page in pager.paginate(Bucket=bucket):
         remote_contents = page['Contents']
         for key in remote_contents:
@@ -43,6 +45,26 @@ def list_bucket_contents(bucket: str):
     print(f"Bucket {bucket} contains {len(all_object_keys)} objects.")
 
     return all_object_keys
+
+def get_graph_file_keys(keys: list):
+    """Given a list of keys, returns a list of those
+    resembling graphs.
+    :param keys: list of object keys, as strings
+    :return: dict of all keys appearing to be graph files,
+            with keys denoting `compressed` or `uncompressed`.
+            Values are lists of strings."""
+    
+    graph_file_keys = {"Compressed":[],"Uncompressed":[]}
+
+    for keyname in keys:
+        if keyname[-7:] == ".tar.gz":
+            graph_file_keys["Compressed"].append(keyname)
+        if keyname[-9:] in ["edges.tsv", "nodes.tsv"]:
+            graph_file_keys["Uncompressed"].append(keyname)
+
+    print(graph_file_keys)
+
+    return graph_file_keys
 
 if __name__ == '__main__':
   run()

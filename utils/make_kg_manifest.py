9961fe78f7516f08b24359052275ffd9b5bb4f6c
caufieldjh <j.harry.caufield@gmail.com>
2022-02-15 15:13:36 -0500

    Track node/edge counts

--- utils/make_kg_manifest.py
+++ utils/make_kg_manifest.py
@@ -48,9 +48,6 @@ PROJECTS = {"kg-obo": "KG-OBO: OBO ontologies into KGX TSV format.",
              "eco-kg": "eco-KG: a knowledge graph of plant traits starting with Planteome and EOL TraitBank.",
              "monarch": "Graph representation of the Monarch Initiative knowledge resource."}
 
-# Temp for testing
-PROJECTS = {"kg-microbe": "KG-Microbe: a knowledge graph for microbial traits."}
-
 # List of component types used to build larger KGs
 SUBGRAPH_TYPES = ["raw",
                 "transformed"]
@@ -487,8 +484,8 @@ def get_stats(bucket: str, data_objects: list):
             and object_type not in ["raw", "transformed"]:
             stats = retrieve_stats(bucket, object_key)
             if stats:
-                print(stats['edge_stats']['total_edges'])
-                print(stats['node_stats']['total_nodes'])
+                object.edge_count = stats['edge_stats']['total_edges']
+                object.node_count = stats['node_stats']['total_nodes']
 
     new_data_objects = data_objects
 

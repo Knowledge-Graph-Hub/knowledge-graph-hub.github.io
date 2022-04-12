d2a82649624c3106ede3372705f74fc3812b87af
caufieldjh <j.harry.caufield@gmail.com>
2022-04-12 15:07:00 -0400

    Have build script prepare config

--- docs/build_site.sh
+++ docs/build_site.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 # Assemble files for KG-Hub site
 
-# Define path to project file
+# Define paths
+JEKYLL_CONFIG_HEADER_FILE="_config_header.yml"
+JEKYLL_CONFIG_FILE="_config.yml"
 PROJECT_FILE="../utils/projects.yaml"
+MANIFEST_URL="https://kg-hub.berkeleybop.io/MANIFEST.yaml"
 
 # Retrieve most recent MANIFEST
-wget https://kg-hub.berkeleybop.io/MANIFEST.yaml
+wget -N $MANIFEST_URL
+
+# Append projects list
+echo "Adding project list to Jekyll config."
+cat $JEKYLL_CONFIG_HEADER_FILE $PROJECT_FILE  > $JEKYLL_CONFIG_FILE

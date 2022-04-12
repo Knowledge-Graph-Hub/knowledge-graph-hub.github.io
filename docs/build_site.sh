1b518e1d9b64bb60d1e245b039b79264dfca70de
caufieldjh <j.harry.caufield@gmail.com>
2022-04-12 15:55:36 -0400

    Add all_graphs table based on MANIFEST

--- docs/build_site.sh
+++ docs/build_site.sh
@@ -6,10 +6,13 @@ JEKYLL_CONFIG_HEADER_FILE="_config_header.yml"
 JEKYLL_CONFIG_FILE="_config.yml"
 PROJECT_FILE="../utils/projects.yaml"
 MANIFEST_URL="https://kg-hub.berkeleybop.io/MANIFEST.yaml"
+MANIFEST_FILE="MANIFEST.yaml"
 
-# Retrieve most recent MANIFEST
+# Retrieve most recent MANIFEST and do some format prep
 wget -N $MANIFEST_URL
+sed -i -e 's/^/  /' $MANIFEST_FILE
+sed -i '1s/^/manifest:\n /' $MANIFEST_FILE
 
 # Append projects list
-echo "Adding project list to Jekyll config."
-cat $JEKYLL_CONFIG_HEADER_FILE $PROJECT_FILE  > $JEKYLL_CONFIG_FILE
+echo "Adding all lists to Jekyll config."
+cat $JEKYLL_CONFIG_HEADER_FILE $PROJECT_FILE $MANIFEST_FILE  > $JEKYLL_CONFIG_FILE

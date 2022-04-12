2266f7767fdfa20e7e0c7a13ad69184273d8f55e
caufieldjh <j.harry.caufield@gmail.com>
2022-04-12 11:55:57 -0400

    Init script for building site w/tables

---
+++ docs/build_site.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Assemble files for KG-Hub site
+
+# Define path to project file
+PROJECT_FILE="../utils/projects.yaml"
+
+# Retrieve most recent MANIFEST
+wget https://kg-hub.berkeleybop.io/MANIFEST.yaml
